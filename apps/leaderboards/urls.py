c9d1c42707cb7f980801fb13194fb833227acac9
Nicolas Frechette <nicolas.frechette@epsb.ca>
2018-12-03 12:09:04 -0700

    Default template for how views could work

--- apps/leaderboards/urls.py
+++ apps/leaderboards/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'leaderboards'
+
 urlpatterns = [
     path('', views.index, name='index'),
+	path('ultimate/', views.ultimateMenu, name='ultimateMenu'),
+	path('ultimate/hrc', views.ultimateMenuHRC, name='ultimateMenuHRC'),
 ]
--- apps/leaderboards/views.py
+++ apps/leaderboards/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("This is the leaderboards page.")
+def index (request):
+    return render(request, 'leaderboards/leaderboards.html')
+	
+def ultimateMenu(request):
+    return render(request, 'leaderboards/ultimate/ultimate.html')
+	
+def ultimateMenuHRC(request):
+    return render(request, 'leaderboards/ultimate/hrc/hrc.html')
